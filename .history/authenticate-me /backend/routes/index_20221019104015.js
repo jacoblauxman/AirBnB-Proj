58d06441feba01a6c3bfbd98ac785c36a0760cc5
Jacob Lauxman <jlauxman@gmail.com>
2022-10-19 12:14:58 -0700

    phase 1 completed

--- .history/authenticate-me /backend/routes/index_20221019104015.js
+++ .history/authenticate-me /backend/routes/index_20221019104015.js
@@ -14,11 +14,4 @@ router.get("/api/csrf/restore", (req, res) => {
 });
 // ...
 
-// backend/routes/index.js
-// ...
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
-// ...
-
 module.exports = router;
