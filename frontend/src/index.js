8e39ef10259fbd181705d78c736ec51d7c459eff
Jacob Lauxman <jlauxman@gmail.com>
2022-11-10 19:22:38 -0700

    Phase 0 of Frontend AuthMe finished

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,33 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 
+// // FRONTEND PHASE 0: call restoreCSRF -- attach custom fetch to window to when in dev
+// import { restoreCSRF, csrfFetch } from './store/csrf'
+
+//TAKE TWO PHASE 0:
+// ... other imports
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+// if (process.env.NODE_ENV !== "production") {
+//   // adding in for PHASE 0 of AUTH ME: checking window of crsfFetch!
+//   restoreCSRF()
+//   window.csrfFetch = csrfFetch;
+//   //template boilerplate:
+//   window.store = store;
+// }
+
+//TAKE TWO : PHASE 0 authMe
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
+
+
 
 function Root() {
   return (
